d9f7875c0acc4ef50dc035986b19106378a6a3b9
Fayziyev Azizbek <104155154+AzizbekFayziyev@users.noreply.github.com>
2022-10-03 17:11:20 +0300

    Updated "dars jadval"

--- src/components/home/sections/Table.jsx
+++ src/components/home/sections/Table.jsx
@@ -29,61 +29,61 @@ export default function Table() {
                 <table className='table'>
                     <tr className={day == 1 ? "actived-day" : null}>
                         <th><i className="fa-solid fa-calendar-days"></i> DUSHANBA</th>
+                        <td><i className="fa-sharp fa-solid fa-book"></i> Ona Tili</td>
+                        <td><i className="fa-sharp fa-solid fa-book"></i> Rus tili</td>
                         <td><i className="fa-sharp fa-solid fa-book"></i> Algebra</td>
-                        <td><i className="fa-sharp fa-solid fa-book"></i> Ona tili</td>
-                        <td><i className="fa-sharp fa-solid fa-book"></i> Informatika</td>
-                        <td><i className="fa-sharp fa-solid fa-book"></i> Kimyo</td>
-                        <td><i className="fa-sharp fa-solid fa-book"></i> Biologiya</td>
+                        <td><i className="fa-sharp fa-solid fa-book"></i> Geometiya</td>
                         <td><i className="fa-sharp fa-solid fa-book"></i> O'.Tarix</td>
+                        <td><i className="fa-sharp fa-solid fa-book"></i> Ingiliz Tili</td>
                     </tr>
 
                     <tr className={day == 2 ? "actived-day" : null}>
                         <th><i className="fa-solid fa-calendar-days"></i> SESHANBA</th>
-                        <td><i className="fa-sharp fa-solid fa-book"></i> Ingliz tili</td>
-                        <td><i className="fa-sharp fa-solid fa-book"></i> J.Tarix</td>
-                        <td><i className="fa-sharp fa-solid fa-book"></i> Adabiyot</td>
-                        <td><i className="fa-sharp fa-solid fa-book"></i> Rus tili</td>
-                        <td><i className="fa-sharp fa-solid fa-book"></i> Biologiya</td>
                         <td><i className="fa-sharp fa-solid fa-book"></i> Fizika</td>
+                        <td><i className="fa-sharp fa-solid fa-book"></i> Kimyo</td>
+                        <td><i className="fa-sharp fa-solid fa-book"></i> Tarbiya</td>
+                        <td><i className="fa-sharp fa-solid fa-book"></i> Biologiya</td>
+                        <td><i className="fa-sharp fa-solid fa-book"></i> J.Tarix</td>
+                        <td><i className="fa-sharp fa-solid fa-book"></i> J.Tarbiya</td>
                     </tr>
 
                     <tr className={day == 3 ? "actived-day" : null}>
                         <th><i className="fa-solid fa-calendar-days"></i> CHORSHANBA</th>
-                        <td><i className="fa-sharp fa-solid fa-book"></i> Chizmachilik</td>
-                        <td><i className="fa-sharp fa-solid fa-book"></i> Biologiya</td>
-                        <td><i className="fa-sharp fa-solid fa-book"></i> Rus tili</td>
-                        <td><i className="fa-sharp fa-solid fa-book"></i> Informatika</td>
-                        <td><i className="fa-sharp fa-solid fa-book"></i> Fizika</td>
-                        <td><i className="fa-sharp fa-solid fa-book"></i> Iqtisod</td>
+                        <td><i className="fa-sharp fa-solid fa-book"></i> Ona Tili</td>
+                        <td><i className="fa-sharp fa-solid fa-book"></i> Adabiyot</td>
+                        <td><i className="fa-sharp fa-solid fa-book"></i> Ingiliz Tili</td>
+                        <td><i className="fa-sharp fa-solid fa-book"></i> O'.Tarix</td>
+                        <td><i className="fa-sharp fa-solid fa-book"></i> Algebra</td>
+                        <td><i className="fa-sharp fa-solid fa-book"></i> Geometriya</td>
                     </tr>
 
                     <tr className={day == 4 ? "actived-day" : null}>
                         <th><i className="fa-solid fa-calendar-days"></i> PAYSHANBA</th>
-                        <td><i className="fa-sharp fa-solid fa-book"></i> O'.Tarix</td>
-                        <td><i className="fa-sharp fa-solid fa-book"></i> Texnologiya</td>
-                        <td><i className="fa-sharp fa-solid fa-book"></i> Rus tili</td>
-                        <td><i className="fa-sharp fa-solid fa-book"></i> Adabiyot</td>
+                        <td><i className="fa-sharp fa-solid fa-book"></i> Huquq</td>
                         <td><i className="fa-sharp fa-solid fa-book"></i> Informatika</td>
+                        <td><i className="fa-sharp fa-solid fa-book"></i> Texnologiya</td>
                         <td><i className="fa-sharp fa-solid fa-book"></i> Geografiya</td>
+                        <td><i className="fa-sharp fa-solid fa-book"></i> Ingiliz Tili</td>
+                        <td><i className="fa-sharp fa-solid fa-book"></i> J.Tarbiya</td>
                     </tr>
 
                     <tr className={day == 5 ? "actived-day" : null}>
                         <th><i className="fa-solid fa-calendar-days"></i> JUMA</th>
                         <td><i className="fa-sharp fa-solid fa-book"></i> Tarbiyaviy.S</td>
-                        <td><i className="fa-sharp fa-solid fa-book"></i> Rus tili</td>
-                        <td><i className="fa-sharp fa-solid fa-book"></i> Fizika</td>
-                        <td><i className="fa-sharp fa-solid fa-book"></i> Adabiyot</td>
                         <td><i className="fa-sharp fa-solid fa-book"></i> Biologiya</td>
+                        <td><i className="fa-sharp fa-solid fa-book"></i> Rus Tili</td>
+                        <td><i className="fa-sharp fa-solid fa-book"></i> Kimyo</td>
+                        <td><i className="fa-sharp fa-solid fa-book"></i> Ona Tili</td>
                         <td><i className="fa-sharp fa-solid fa-book"></i> Informatika</td>
                     </tr>
 
                     <tr className={day == 6 ? "actived-day" : null}>
                         <th><i className="fa-solid fa-calendar-days"></i> SHANBA</th>
-                        <td><i className="fa-sharp fa-solid fa-book"></i> Ona tili</td>
                         <td><i className="fa-sharp fa-solid fa-book"></i> Geografiya</td>
-                        <td><i className="fa-sharp fa-solid fa-book"></i> Biologiya</td>
-                        <td><i className="fa-sharp fa-solid fa-book"></i> Tarbiya</td>
-                        <td><i className="fa-sharp fa-solid fa-book"></i> Rus tili</td>
+                        <td><i className="fa-sharp fa-solid fa-book"></i> Adabiyot</td>
+                        <td><i className="fa-sharp fa-solid fa-book"></i> Chizmachilik</td>
+                        <td><i className="fa-sharp fa-solid fa-book"></i> Algebra</td>
+                        <td><i className="fa-sharp fa-solid fa-book"></i> Fizika</td>
                     </tr>
 
                 </table>
